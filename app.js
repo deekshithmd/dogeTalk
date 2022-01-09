9344e10b589a9e9cf61d78af7f72859f3566c38e
Deekshith M D <deekshithmogra@gmail.com>
2022-01-09 17:30:32 +0530

    refactor : es6

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-var btn=document.querySelector(".btn-translate")
-var text=document.querySelector("#input")
-var output=document.querySelector(".translated")
+const btn=document.querySelector(".btn-translate")
+const text=document.querySelector("#input")
+const output=document.querySelector(".translated")
 //output.innerText="Hi"
 const url="https://api.funtranslations.com/translate/doge.json"
 
 const constructUrl= text => url+"?"+"text="+text;
 
-const errorHandle=()=>{
-    alert("Some error occured...Please try after some time")
-}
+const errorHandle=()=> alert("Some error occured...Please try after some time");
 
-const translateDoge=()=>{
-    var inputText=text.value
+const translateDoge = () =>{
+    let inputText=text.value
     fetch(constructUrl(inputText))
     .then(response=>response.json())
     .then(json=>output.innerText=json.contents.translated)
